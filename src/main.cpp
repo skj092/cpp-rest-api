a1c6f9c026eb05a24906f5ded942bbfd23d49e9c
skj092 <sonujha092@gmail.com>
2024-01-20 15:30:49 +0530

    post request

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <pistache/endpoint.h>
+#include <iostream>
 
 using namespace Pistache;
 
 struct HelloHandler : public Http::Handler {
-  HTTP_PROTOTYPE(HelloHandler)
-  void onRequest(const Http::Request&, Http::ResponseWriter writer) override {
-    writer.send(Http::Code::Ok, "Hello, World!, this is a test");
-  }
+    HTTP_PROTOTYPE(HelloHandler)
+
+    void onRequest(const Http::Request& request, Http::ResponseWriter response) override {
+        if (request.method() == Http::Method::Post) {
+            // Handle the POST request here
+            response.send(Http::Code::Ok, "Received a POST request: " + request.body());
+        } else {
+            response.send(Http::Code::Ok, "Hello, World!, this is a test");
+        }
+    }
 };
 
 int main() {
-  Http::listenAndServe<HelloHandler>(Pistache::Address("*:9080"));
+    Http::listenAndServe<HelloHandler>(Pistache::Address("*:9080"));
+    return 0;
 }
